fc5903b3114779cde49d53c535f364707723cd47
Raphael Clemente Miranda <raphaelc484@hotmail.com>
2021-03-19 12:11:18 -0300

    mission accomplished

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      if (Array.isArray(user_id)) {
+        throw Error("User idenfier must be a string");
+      }
+
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userFind = this.usersRepository.findById(user_id);
+
+    if (!userFind) {
+      throw new Error("User not found!");
+    }
+
+    return userFind;
   }
 }
 
